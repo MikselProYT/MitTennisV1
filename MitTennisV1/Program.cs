2082ec735415405c3e691e9d3806cf80465ab214
MikselProYT <mikkellengaard@gmail.com>
2026-08-14 13:36:42 +0200

    Add project files.

---
+++ MitTennisV1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program navn: Tennis
+            // Programbeskrivelse: Dette program er et spil, hvor brugeren kan vælge mellem at spille pingpong eller tennis. Programmet vil derefter starte det valgte spil.
+
+            // Fortatter: Mikkel Lengård.
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hej Bruger dette er pingpong eller Tennis");
+            Console.SetCursorPosition(0, Console.CursorTop + 1);
+            Console.WriteLine("Skriv 'pingpong' for at spille pingpong eller 'tennis' for at spille tennis");
+            Thread.Sleep(10000000);
+            
+            
+        }
+    }
+}
